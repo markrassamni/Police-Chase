509ccf9cea17ba801ce795ca0e1a9918302b51dc
Mark <markrassamni@me.com>
2017-11-01 21:45:48 -0700

    init

---
+++ Assets/SceneController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+	void Start () {
+		DontDestroyOnLoad(gameObject);
+		SceneManager.LoadScene("MainMenu");
+	}
+	
+	void Update () {
+		
+	}
+
+	public void LoadGame(){
+		SceneManager.LoadScene("Game");
+	}
+}

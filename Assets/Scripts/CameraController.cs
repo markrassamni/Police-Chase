bcc14abb820d51d44c546a8fe50a5e3e88598e46
Mark <markrassamni@me.com>
2017-11-03 00:55:26 -0700

    character moves with player control

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,17 +4,9 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-	[SerializeField] private GameObject car;
 	[SerializeField] private float moveSpeed;
-	private Vector3 carPosition;
-	private float cameraOffset;
-
-	void Start () {
-		
-	}
 	
 	void Update () {
-		carPosition = car.transform.position;
 		transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
 	}
 }
--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -5,45 +5,30 @@ using UnityEngine;
 public class CarController : MonoBehaviour {
 
 	[SerializeField] private float moveSpeed;
-	//TODO make min/max consts
-	[SerializeField] private float maxY;
-	[SerializeField] private float minY;
 	[SerializeField] private float minX;
 	[SerializeField] private float maxX;
-	private float locationOffset = 0f;
-	
-	void Start () {
-		
-	}
+	private const float offsetFromSide = 0.8f;
+	private const float minY = -4.2f;
+	private const float maxY = 4.2f;
 	
 	void Update () {
+		minX = Camera.main.ViewportToWorldPoint(Vector3.zero).x + offsetFromSide;
+		maxX = Camera.main.transform.position.x - offsetFromSide;
 		HandleUserInput();
-	}
-
-	void LateUpdate(){
-		MoveCar();
-	}
-
-	private void MoveCar(){
-		Vector3 lowerBound = Camera.main.ViewportToWorldPoint(Vector3.zero);
-		Vector3 upperBound = Camera.main.ViewportToWorldPoint(Vector3.one);
-		transform.position = new Vector3(lowerBound.x + locationOffset, transform.position.y, transform.position.z);
 	}
 
 	private void HandleUserInput(){
 		if (Input.GetKey("left") || Input.GetKey("a")){
-			print("left");
+			transform.position = Vector3.MoveTowards(transform.position, new Vector3(minX, transform.position.y, transform.position.z), moveSpeed * Time.deltaTime);
 		}
 		if (Input.GetKey("right") || Input.GetKey("d")){
-			print("right");
+			transform.position = Vector3.MoveTowards(transform.position, new Vector3(maxX, transform.position.y, transform.position.z), moveSpeed * Time.deltaTime);
 		}
 		if (Input.GetKey("up") || Input.GetKey("w")){
-			transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, maxY, transform.position.z),
-				moveSpeed * Time.deltaTime);
+			transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, maxY, transform.position.z), moveSpeed * Time.deltaTime);
 		}
 		if (Input.GetKey("down") || Input.GetKey("s")){
-			transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, minY, transform.position.z),
-				moveSpeed * Time.deltaTime);
+			transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, minY, transform.position.z), moveSpeed * Time.deltaTime);
 		}
 	}
 }

13e5654b57bc51db1ca16a74fdd36eb89127d8e6
Mark <markrassamni@me.com>
2017-11-03 04:30:30 -0700

    added roadcontroller to scroll background

---
+++ Assets/Scripts/RoadController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadController : MonoBehaviour {
+
+	[SerializeField] private float moveSpeed;
+	private Vector2 originalOffset;
+
+	void Start () {
+		originalOffset = GetComponent<Renderer>().sharedMaterial.GetTextureOffset("_MainTex");
+	}
+	
+	void Update () {
+		float newOffsetX = Mathf.Repeat(Time.time * moveSpeed, 1);
+		GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", new Vector2(newOffsetX, originalOffset.y));
+	}
+
+	void OnDisable(){
+		GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", originalOffset);
+	}
+}

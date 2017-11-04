1a3ad3f338c520bef4db99c30ba7e15d378ffcbc
Mark <markrassamni@me.com>
2017-11-03 22:35:33 -0700

    spawn and move obstacles

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+	[SerializeField] private float moveSpeed;
+
+	public float MoveSpeed {
+		get{
+			return moveSpeed;
+		}
+	}
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "ObstacleDestroyer"){
+			FindObjectOfType<ObstacleController>().DestoryObstacle(this);
+		}
+	}
+}
---
+++ Assets/Scripts/ObstacleController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour {
+
+	[SerializeField] private float spawnDelay;
+	[SerializeField] private Obstacle[] obstaclePrefabs;
+	[SerializeField] private Transform[] spawnPoints;
+	private List<Obstacle> obstacles = new List<Obstacle>();
+
+	IEnumerator Start () {
+		yield return new WaitForSeconds(1.5f);
+		StartCoroutine(Spawn());
+	}
+	
+	void Update () {
+		MoveObstacles();
+	}
+
+	private IEnumerator Spawn(){
+		Transform randomSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+		Obstacle randomObstacle = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
+		Obstacle obstacle = Instantiate(randomObstacle, randomSpawnPoint) as Obstacle;
+		obstacles.Add(obstacle);
+		yield return new WaitForSeconds(spawnDelay);
+		StartCoroutine(Spawn());
+	}
+
+	private void MoveObstacles(){
+		foreach(Obstacle obstacle in obstacles){
+			float moveSpeed = obstacle.MoveSpeed;
+			obstacle.transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+		}
+	}
+
+	public void DestoryObstacle(Obstacle obstacle){
+		obstacles.Remove(obstacle);
+		Destroy(obstacle.gameObject);
+	}
+}

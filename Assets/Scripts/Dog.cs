bd581dac2ad231e74ea90f9bff1972244d715cbd
Mark <markrassamni@me.com>
2017-11-03 23:22:16 -0700

    added running dog

---
+++ Assets/Scripts/Dog.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : Obstacle {
+
+	[SerializeField] private float changeSpriteSpeed;
+	[SerializeField] private Sprite[] sprites;
+	private SpriteRenderer spriteRenderer;
+	private int currentSprite = 1;
+
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		StartCoroutine(Animate());
+	}
+	
+	private IEnumerator Animate(){
+		yield return new WaitForSeconds(changeSpriteSpeed);
+		if (currentSprite < sprites.Length){
+			currentSprite += 1;
+		} else {
+			currentSprite = 1;
+		}
+		spriteRenderer.sprite = sprites[currentSprite-1];
+		StartCoroutine(Animate());
+	}
+}

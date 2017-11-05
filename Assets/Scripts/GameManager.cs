99f81792b513997781faae387a95e85d825fed32
Mark <markrassamni@me.com>
2017-11-04 17:48:17 -0700

    added gamemanager singleton

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	private GameManager instance;
+	public GameManager Instance { get { return instance; } }
+
+	void Start () {
+		if (instance != null && instance != this){
+            Destroy(this.gameObject);
+        } else {
+            instance = this;
+        }
+	}
+	
+	void Update () {
+		
+	}
+}

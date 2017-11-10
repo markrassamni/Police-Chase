2e9174e32620d5b3d78219b6bfe7667a5183d450
Mark <markrassamni@me.com>
2017-11-09 20:34:21 -0800

    added sound controller

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : Singleton<SoundController> {
+
+	[SerializeField] private AudioClip menuMusic;
+	[SerializeField] private AudioClip gameMusic;
+
+	override protected void Awake(){
+		base.Awake();
+		DontDestroyOnLoad(this);
+	}
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}

9d58d21b3404e027cc008220d13db9a7fc2d2acf
Mark <markrassamni@me.com>
2017-11-26 12:53:39 -0800

    scroll the credits

--- Assets/Scripts/CreditsController.cs
+++ Assets/Scripts/CreditsController.cs
@@ -1,7 +1,39 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class CreditsController : MonoBehaviour {
+
+	[SerializeField] private RectTransform content;
+	[SerializeField] private float scrollSpeed;
+	private const float scrollDelay = 1f;
+	private const float maxScroll = 2610.774f;
+	private bool stopScrolling;
+	private bool startScrolling;
+
+	IEnumerator Start(){
+		// start scrolling after delay
+		yield return new WaitForSeconds(scrollDelay);
+		startScrolling = true;
+	}
+
+	void Update(){
+		// scroll until any user input
+		if (Input.anyKeyDown || Input.GetAxis("Mouse ScrollWheel") != 0f){
+			stopScrolling = true;
+		} 
+		if(startScrolling && !stopScrolling){
+			Scroll();
+		}
+	}
+
+	private void Scroll(){
+		content.anchoredPosition += new Vector2(0f, scrollSpeed);
+		if (content.anchoredPosition.y >= maxScroll){
+			stopScrolling = true;
+			content.anchoredPosition = new Vector2(content.anchoredPosition.x, maxScroll);
+		}
+	}
 
 	public void OpenURL(Text txt){
 		string url = txt.text.Replace("\n", "").Replace("\r", "");

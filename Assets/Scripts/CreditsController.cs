953cbb61067c99499a10412076ef2443554b6733
Mark <markrassamni@me.com>
2017-11-26 00:58:59 -0800

    pressing URL in credits opens link in browser

---
+++ Assets/Scripts/CreditsController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsController : MonoBehaviour {
+
+	public void OpenURL(Text txt){
+		string url = txt.text.Replace("\n", "").Replace("\r", "");
+		Application.OpenURL(url);
+	}
+
+}
